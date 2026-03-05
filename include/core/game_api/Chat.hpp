5417de5b3c8f9ee255ae714b9c42831756fd0047
RedShyGuy <luk2003.lm@gmail.com>
2026-03-05 11:05:34 +0100

    added foofoos chat edits and refactored them

--- include/core/game_api/Chat.hpp
+++ include/core/game_api/Chat.hpp
@@ -11,31 +11,12 @@
 
 namespace CTRPluginFramework {
 	class Chat {
-        public:
-			u32 GetPlayerMessageData(void);
-			std::string GetPlayerMessage(void);
-			void ClearPlayerMessage(void);
-			std::string GetPlayerName(void);
-			u8 GetPlayerIndex(void);
-			bool IsPlayerMessageOnScreen(void);
-            static void EnableCommands(void);
-            static void DisableCommands(void);
+	public:
+		static void EnableCommands(void);
+		static void DisableCommands(void);
 
-        protected:
-            static void CommandCallback(void);
-
-        private:    
-            void CommandLoop(void);
-			void AnimationCommand(void);
-			void EmotionCommand(void);
-			void SnakeCommand(void);
-			void MusicCommand(void);
-			void ItemCommand(void);
-
-			u8 animID = 6;
-			u8 emotionID = 1;
-			u16 snakeID = 1;
-			u16 musicID = 0x660;
-			CTRPluginFramework::Item itemID = { 0x7FFE, 0 };
+	private:
+		static void CommandCallback(void);
+		static void ProcessChatCommand(void);
 	};
 }
--- src/core/game_api/Chat.cpp
+++ src/core/game_api/Chat.cpp
@@ -1,220 +1,381 @@
 #include <CTRPluginFramework.hpp>
 #include "core/game_api/Chat.hpp"
+#include <cstring>
 
 namespace CTRPluginFramework {
-	u32 Chat::GetPlayerMessageData() {
-		u8 _pID = Game::GetActualPlayerIndex();
-		// swap your index with player 0 in order to get the correct pointer
-		if(_pID == Game::GetOnlinePlayerIndex()) {
-			_pID = 0;
-		}
-		else if(_pID == 0) {
-			_pID = Game::GetOnlinePlayerIndex();
-		}
+    namespace {
+        constexpr const char *kAnimationCommand = "a:";
+        constexpr const char *kEmotionCommand = "e:";
+        constexpr const char *kSnakeCommand = "s:";
+        constexpr const char *kMusicCommand = "m:";
+        constexpr const char *kItemCommand = "i:";
+        constexpr const char *kNameCommand = "n:";
+        constexpr const char *kItemFlagCommand = "f:";
 
-	    u32 PTR = *(u32 *)Address(0x94FD84).addr; //0x94FD84
-		PTR += 0x464; //33078FA0
-		PTR += (0x530 * _pID);
-		return PTR;
-	}
+        struct RuntimeState {
+            Item item = {0x7FFE, 0};
+        };
 
-	std::string Chat::GetPlayerMessage() {
-        std::string pMessage;
-        Process::ReadString(GetPlayerMessageData() + 0x40C, pMessage, 0x82, StringFormat::Utf16);
-		return pMessage;
-	}
+        struct ParsedCommand {
+            std::string command;
+            std::string specialCommand;
+            std::string ID_8Bit;
+            std::string ID_12Bit;
+            std::string ID_16Bit;
+            std::string SPID_16Bit;
+            std::string itemName;
+        };
 
-	void Chat::ClearPlayerMessage() {
-		std::memset((void *)(GetPlayerMessageData() + 0x40C), 0, 0x82);
-	}
+        RuntimeState gState;
 
-	std::string Chat::GetPlayerName() {
-        std::string pName;
-        Process::ReadString(GetPlayerMessageData() + 0x3FA, pName, 0x16, StringFormat::Utf16);
-		return pName;
-	}
+        std::string ToLowerCopy(std::string text) {
+            UtilsExtras::ConvertToLowcase(text);
+            return text;
+        }
 
-	u8 Chat::GetPlayerIndex() {
-		return *(u8 *)(GetPlayerMessageData() + 0x3EC);
-	}
+        u32 PackItemForDisplay(const Item &item) {
+            return (static_cast<u32>(item.Flags) << 16) | item.ID;
+        }
 
-	bool Chat::IsPlayerMessageOnScreen() {
-		u8 Short;
-		Process::Read8(GetPlayerMessageData() + 0x3F0, Short);
-		return (Short != 0);
-	}
+        u32 ResolvePlayerMessageDataAddress(void) {
+            u8 playerIndex = Game::GetActualPlayerIndex();
+            const u8 onlinePlayerIndex = Game::GetOnlinePlayerIndex();
 
-	void Chat::AnimationCommand() {
-		u32 x, y;
-		if(PlayerClass::GetInstance()->GetWorldCoords(&x, &y)) {
-			Animation::ExecuteAnimationWrapper(GetPlayerIndex(), animID, {0, 0}, 0, 0, 0, 0, x, y, true);
-			Sleep(Seconds(2));
-			Animation::ExecuteAnimationWrapper(GetPlayerIndex(), 6, {0, 0}, 0, 0, 0, 0, x, y, true);
+            if (playerIndex == onlinePlayerIndex) {
+                playerIndex = 0;
+            } else if (playerIndex == 0) {
+                playerIndex = onlinePlayerIndex;
+            }
 
-			OSD::NotifySysFont(Utils::Format(Language::getInstance()->get(TextID::CHAT_ANIMATION).c_str(), animID));
-		}
-	}
+            u32 playerData = *reinterpret_cast<u32 *>(Address(0x94FD84).addr);
+            playerData += 0x464;
+            playerData += (0x530 * playerIndex);
 
-	void Chat::EmotionCommand() {
-		u32 x, y;
-		if(PlayerClass::GetInstance()->GetWorldCoords(&x, &y)) {
-			Animation::ExecuteAnimationWrapper(GetPlayerIndex(), 0xAF, {0, 0}, emotionID, 0, 0, 0, x, y, true);
-			Sleep(Seconds(2));
-			Animation::ExecuteAnimationWrapper(GetPlayerIndex(), 6, {0, 0}, 0, 0, 0, 0, x, y, true);
+            return playerData;
+        }
 
-			OSD::NotifySysFont(Utils::Format(Language::getInstance()->get(TextID::CHAT_EMOTION).c_str(), emotionID));
-		}
-	}
+        std::string ReadPlayerMessage(const u32 messageDataAddress) {
+            std::string playerMessage;
+            Process::ReadString(
+                messageDataAddress + 0x40C,
+                playerMessage,
+                0x82,
+                StringFormat::Utf16
+            );
+            return playerMessage;
+        }
 
-	void Chat::SnakeCommand() {
-		u32 x, y;
-		if(PlayerClass::GetInstance()->GetWorldCoords(&x, &y)) {
-			Animation::ExecuteAnimationWrapper(GetPlayerIndex(), 0xC5, {0, 0}, 0, snakeID, 0, 0, x, y, true);
-			Sleep(Seconds(2));
-			Animation::ExecuteAnimationWrapper(GetPlayerIndex(), 6, {0, 0}, 0, 0, 0, 0, x, y, true);
+        void ClearPlayerMessage(const u32 messageDataAddress) {
+            std::memset(reinterpret_cast<void *>(messageDataAddress + 0x40C), 0, 0x82);
+        }
 
-			OSD::NotifySysFont(Utils::Format(Language::getInstance()->get(TextID::CHAT_SNAKE).c_str(), snakeID));
-		}
-	}
+        u8 ReadPlayerIndex(const u32 messageDataAddress) {
+            return *reinterpret_cast<u8 *>(messageDataAddress + 0x3EC);
+        }
 
-	void Chat::MusicCommand() {
-		u32 x, y;
-		if(PlayerClass::GetInstance()->GetWorldCoords(&x, &y)) {
-			Animation::ExecuteAnimationWrapper(GetPlayerIndex(), 0xC4, {0, 0}, 0, 0, musicID, 0, x, y, true);
-			Sleep(Milliseconds(100));
-			Animation::ExecuteAnimationWrapper(GetPlayerIndex(), 6, {0, 0}, 0, 0, 0, 0, x, y, true);
+        bool IsPlayerMessageOnScreen(const u32 messageDataAddress) {
+            u8 isVisible = 0;
+            Process::Read8(messageDataAddress + 0x3F0, isVisible);
+            return (isVisible != 0);
+        }
 
-			OSD::NotifySysFont(Utils::Format(Language::getInstance()->get(TextID::CHAT_MUSIC).c_str(), musicID));
-		}
-	}
+        bool TryGetWorldCoords(u32 &x, u32 &y) {
+            return PlayerClass::GetInstance()->GetWorldCoords(&x, &y);
+        }
 
-	void Chat::ItemCommand() {
-		u32 x, y;
-		if(PlayerClass::GetInstance()->GetWorldCoords(&x, &y)) {
-			Dropper::PlaceItemWrapper(0xA, ReplaceEverything, &itemID, &itemID, x, y, 0, 0, 0, 0, 0, 0x56, 0xA5, false);
+        void ExecuteAnimationWithReset(
+            const u8 playerIndex,
+            const u8 animationId,
+            const u16 emotionId,
+            const u16 snakeId,
+            const u16 musicId,
+            const u32 x,
+            const u32 y,
+            const u32 resetDelayMs
+        ) {
+            Animation::ExecuteAnimationWrapper(
+                playerIndex,
+                animationId,
+                {0, 0},
+                emotionId,
+                snakeId,
+                musicId,
+                0,
+                x,
+                y,
+                true
+            );
 
-			OSD::NotifySysFont(Utils::Format(Language::getInstance()->get(TextID::CHAT_ITEM).c_str(), *(u32*)&itemID));
-		}
-	}
+            Sleep(Milliseconds(resetDelayMs));
 
-	void Chat::CommandLoop() {
-		if(!IsPlayerMessageOnScreen()) {
-			return;
-		}
+            Animation::ExecuteAnimationWrapper(
+                playerIndex,
+                6,
+                {0, 0},
+                0,
+                0,
+                0,
+                0,
+                x,
+                y,
+                true
+            );
+        }
 
-		std::string PlayerText = GetPlayerMessage();
+        ParsedCommand ParseCommandText(std::string playerText) {
+            playerText.resize(25, ' ');
 
-		if(PlayerText.empty()) {
-			return;
-		}
+            ParsedCommand parsed{};
+            parsed.command = ToLowerCopy(playerText.substr(0, 2));
+            parsed.specialCommand = ToLowerCopy(playerText.substr(6, 2));
+            parsed.ID_8Bit = playerText.substr(2, 2);
+            parsed.ID_12Bit = playerText.substr(2, 3);
+            parsed.ID_16Bit = playerText.substr(2, 4);
+            parsed.SPID_16Bit = playerText.substr(8, 4);
+            parsed.itemName = playerText.substr(2, 23);
 
-		PlayerText.resize(25, ' ');
+            return parsed;
+        }
 
-	//command
-		std::string Command = PlayerText.substr(0, 2);
-		UtilsExtras::ConvertToLowcase(Command);
-	//special case command (flag)
-		std::string SPCommand = PlayerText.substr(6, 2);
-		UtilsExtras::ConvertToLowcase(SPCommand);
-	//ID's
-		std::string ID_8Bit = PlayerText.substr(2, 2);
-		std::string ID_12Bit = PlayerText.substr(2, 3);
-		std::string ID_16Bit = PlayerText.substr(2, 4);
+        bool TryExecuteAnimationCommand(
+            const u8 playerIndex,
+            const u8 animationId,
+            const u16 emotionId,
+            const u16 snakeId,
+            const u16 musicId,
+            const u32 resetDelayMs
+        ) {
+            u32 x, y;
+            if (!TryGetWorldCoords(x, y)) {
+                return false;
+            }
 
-	//special case ID (flag)
-		std::string SPID_16Bit = PlayerText.substr(8, 4);
+            ExecuteAnimationWithReset(playerIndex, animationId, emotionId, snakeId, musicId, x, y, resetDelayMs);
+            return true;
+        }
 
-	//Item Name
-		std::string ItemName = PlayerText.substr(2, 23);
+        bool TryExecuteItemCommand(Item &item) {
+            u32 x, y;
+            if (!TryGetWorldCoords(x, y)) {
+                return false;
+            }
 
-		if(Command == "a:") {
-			animID = StringToHex<u8>(ID_8Bit, 6); //sets animation
-			if(IDChecks::AnimationValid(animID, GetPlayerIndex())) {
-				AnimationCommand();
-			}
-			else {
-				OSD::NotifySysFont(Language::getInstance()->get(TextID::CHAT_INVALID_ANIMATION), Color::Red);
+            Dropper::PlaceItemWrapper(
+                0xA,
+                ReplaceEverything,
+                &item,
+                &item,
+                x,
+                y,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0x56,
+                0xA5,
+                false
+            );
+
+            return true;
+        }
+
+        void HandleAnimationCommand(const ParsedCommand &parsed, const u8 playerIndex) {
+            const u8 animID = StringToHex<u8>(parsed.ID_8Bit, 6);
+            if (!IDChecks::AnimationValid(animID, playerIndex)) {
+                OSD::NotifySysFont(
+                    Language::getInstance()->get(TextID::CHAT_INVALID_ANIMATION),
+                    Color::Red
+                );
                 return;
             }
-		}
 
-		else if(Command == "e:") {
-			emotionID = StringToHex<u8>(ID_8Bit, 1); //sets emotion
-			if(IDChecks::EmotionValid(emotionID)) {
-				EmotionCommand();
-			}
-			else {
-				OSD::NotifySysFont(Language::getInstance()->get(TextID::CHAT_INVALID_EMOTION), Color::Red);
+            if (TryExecuteAnimationCommand(playerIndex, animID, 0, 0, 0, 2000)) {
+                OSD::NotifySysFont(
+                    Utils::Format(
+                        Language::getInstance()->get(TextID::CHAT_ANIMATION).c_str(),
+                        animID
+                    )
+                );
+            }
+
+        }
+
+        void HandleEmotionCommand(const ParsedCommand &parsed, const u8 playerIndex) {
+            const u8 emotionID = StringToHex<u8>(parsed.ID_8Bit, 1);
+            if (!IDChecks::EmotionValid(emotionID)) {
+                OSD::NotifySysFont(
+                    Language::getInstance()->get(TextID::CHAT_INVALID_EMOTION),
+                    Color::Red
+                );
                 return;
             }
-		}
 
-		else if(Command == "s:") {
-			snakeID = StringToHex<u16>(ID_12Bit, 1); //sets snake
-			if(IDChecks::SnakeValid(snakeID)) {
-				SnakeCommand();
-			}
-			else {
-				OSD::NotifySysFont(Language::getInstance()->get(TextID::CHAT_INVALID_SNAKE), Color::Red);
+            if (TryExecuteAnimationCommand(playerIndex, 0xAF, emotionID, 0, 0, 2000)) {
+                OSD::NotifySysFont(
+                    Utils::Format(
+                        Language::getInstance()->get(TextID::CHAT_EMOTION).c_str(),
+                        emotionID
+                    )
+                );
+            }
+
+        }
+
+        void HandleSnakeCommand(const ParsedCommand &parsed, const u8 playerIndex) {
+            const u16 snakeID = StringToHex<u16>(parsed.ID_12Bit, 1);
+            if (!IDChecks::SnakeValid(snakeID)) {
+                OSD::NotifySysFont(
+                    Language::getInstance()->get(TextID::CHAT_INVALID_SNAKE),
+                    Color::Red
+                );
                 return;
             }
-		}
 
-		else if(Command == "m:") {
-			musicID = StringToHex<u16>(ID_12Bit, 0x660); //sets music
-			if(IDChecks::MusicValid(musicID)) {
-				MusicCommand();
-			}
-			else {
-				OSD::NotifySysFont(Language::getInstance()->get(TextID::CHAT_INVALID_MUSIC), Color::Red);
+            if (TryExecuteAnimationCommand(playerIndex, 0xC5, 0, snakeID, 0, 2000)) {
+                OSD::NotifySysFont(
+                    Utils::Format(
+                        Language::getInstance()->get(TextID::CHAT_SNAKE).c_str(),
+                        snakeID
+                    )
+                );
+            }
+
+        }
+
+        void HandleMusicCommand(const ParsedCommand &parsed, const u8 playerIndex) {
+            const u16 musicID = StringToHex<u16>(parsed.ID_12Bit, 0x660);
+            if (!IDChecks::MusicValid(musicID)) {
+                OSD::NotifySysFont(
+                    Language::getInstance()->get(TextID::CHAT_INVALID_MUSIC),
+                    Color::Red
+                );
                 return;
             }
-		}
 
-		else if(Command == "i:") {
-			itemID.ID = StringToHex<u16>(ID_16Bit, 0x2001); //sets item
-			if(itemID.isValid()) {
-				if(SPCommand == "f:") {
-					itemID.Flags = StringToHex<u16>(SPID_16Bit, 0); //sets flag
-				}
-				ItemCommand();
-			}
-			else {
-				OSD::NotifySysFont(Language::getInstance()->get(TextID::INVALID_ITEM), Color::Red);
-				return;
-			}
-		}
+            if (TryExecuteAnimationCommand(playerIndex, 0xC4, 0, 0, musicID, 100)) {
+                OSD::NotifySysFont(
+                    Utils::Format(
+                        Language::getInstance()->get(TextID::CHAT_MUSIC).c_str(),
+                        musicID
+                    )
+                );
+            }
 
-		else if(Command == "n:") {
-			UtilsExtras::Trim(ItemName);
-			ItemNamePack match;
-			if (!Item::searchByKeyword(ItemName, match)) {
-				OSD::NotifySysFont(Language::getInstance()->get(TextID::CHAT_NO_ITEM_FOUND), Color::Red);
-				return;
-			}
+        }
 
-			itemID = Item(match.ID); //sets item
-			if(!itemID.isValid()) { //should always be true if orig file is used
-				OSD::NotifySysFont(Language::getInstance()->get(TextID::INVALID_ITEM), Color::Red);
-				return;
-			}
+        void HandleItemCommand(const ParsedCommand &parsed) {
+            gState.item.ID = StringToHex<u16>(parsed.ID_16Bit, 0x2001);
+            if (!gState.item.isValid()) {
+                OSD::NotifySysFont(
+                    Language::getInstance()->get(TextID::INVALID_ITEM),
+                    Color::Red
+                );
+                return;
+            }
 
-			ItemCommand();
-		}
+            if (parsed.specialCommand == kItemFlagCommand) {
+                gState.item.Flags = StringToHex<u16>(parsed.SPID_16Bit, 0);
+            }
 
-		else { // any other message
-			return;
-		}
-		ClearPlayerMessage();
-	}
+            if (TryExecuteItemCommand(gState.item)) {
+                OSD::NotifySysFont(
+                    Utils::Format(
+                        Language::getInstance()->get(TextID::CHAT_ITEM).c_str(),
+                        PackItemForDisplay(gState.item)
+                    )
+                );
+            }
+
+        }
+
+        void HandleNameCommand(ParsedCommand parsed) {
+            UtilsExtras::Trim(parsed.itemName);
+
+            ItemNamePack match;
+            if (!Item::searchByKeyword(parsed.itemName, match)) {
+                OSD::NotifySysFont(
+                    Language::getInstance()->get(TextID::CHAT_NO_ITEM_FOUND),
+                    Color::Red
+                );
+                return;
+            }
+
+            gState.item = Item(match.ID);
+            if (!gState.item.isValid()) {
+                OSD::NotifySysFont(
+                    Language::getInstance()->get(TextID::INVALID_ITEM),
+                    Color::Red
+                );
+                return;
+            }
+
+            if (TryExecuteItemCommand(gState.item)) {
+                OSD::NotifySysFont(
+                    Utils::Format(
+                        Language::getInstance()->get(TextID::CHAT_ITEM).c_str(),
+                        PackItemForDisplay(gState.item)
+                    )
+                );
+            }
+
+        }
+
+        bool ExecuteParsedCommand(const ParsedCommand &parsed, const u8 playerIndex) {
+            if (parsed.command == kAnimationCommand) {
+                HandleAnimationCommand(parsed, playerIndex);
+                return true;
+            }
+            if (parsed.command == kEmotionCommand) {
+                HandleEmotionCommand(parsed, playerIndex);
+                return true;
+            }
+            if (parsed.command == kSnakeCommand) {
+                HandleSnakeCommand(parsed, playerIndex);
+                return true;
+            }
+            if (parsed.command == kMusicCommand) {
+                HandleMusicCommand(parsed, playerIndex);
+                return true;
+            }
+            if (parsed.command == kItemCommand) {
+                HandleItemCommand(parsed);
+                return true;
+            }
+            if (parsed.command == kNameCommand) {
+                HandleNameCommand(parsed);
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    void Chat::ProcessChatCommand(void) {
+        const u32 messageDataAddress = ResolvePlayerMessageDataAddress();
+        if (!IsPlayerMessageOnScreen(messageDataAddress)) {
+            return;
+        }
+
+        const std::string playerMessage = ReadPlayerMessage(messageDataAddress);
+        if (playerMessage.empty()) {
+            return;
+        }
+
+        const ParsedCommand parsed = ParseCommandText(playerMessage);
+        const u8 playerIndex = ReadPlayerIndex(messageDataAddress);
+
+        if (ExecuteParsedCommand(parsed, playerIndex)) {
+            ClearPlayerMessage(messageDataAddress);
+        }
+    }
 
     void Chat::CommandCallback(void) {
-		if (!PlayerClass::GetInstance()->IsLoaded()) {
-			return;
-		}
+        if (!PlayerClass::GetInstance()->IsLoaded()) {
+            return;
+        }
 
-        Chat chat;
-        chat.CommandLoop();
+        ProcessChatCommand();
     }
 
     void Chat::EnableCommands(void) {
